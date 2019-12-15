799ca3285696c2618445af0c7738c7a13f13f1ab
cn <cn@mycrobase.de>
2019-12-15 23:12:15 +0100

    module: support Python 3 only

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ from setuptools import setup
 setup(name='bme280',
       version='1.0.0',
       packages=['bme280'],
-
       install_requires=['smbus2'],
-      python_requires='>=2.7',
-
+      python_requires='>=3.4',
       url='https://dev.mycrobase.de/gitea/cn/python-bme280',
       author='Christian Nicolai',
       description='A python library for accessing the BME280 combined humidity and pressure sensor from Bosch.',
